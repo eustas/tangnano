0d52e39b0e08fa1176bb71d7a852dc8ae414ef6e
Evgenii Kliuchnikov <eustas.ru@gmail.com>
2024-04-30 11:39:37 +0200

    Initial devcontainer

---
+++ .devcontainer/install-deps.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt-get update
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get -y install \
+    clang \
+    clang-format \
+    cmake \
+    curl \
+    git \
+    libboost-all-dev \
+    libeigen3-dev \
+    python3-dev \
+    qt5-qmake \
+    qtbase5-dev \
+    qtcreator
