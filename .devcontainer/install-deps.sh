4772b263f12f2efa0ee98101f32fe3b7035fd2da
Evgenii Kliuchnikov <eustas.ru@gmail.com>
2024-04-30 14:25:43 +0200

    make yosys

--- .devcontainer/install-deps.sh
+++ .devcontainer/install-deps.sh
@@ -5,23 +5,47 @@ apt-get update
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get -y install python3-dev python3-pip
-pip3 install apycula
+mkdir /opt/src
+
+#apt-get -y install python3-dev python3-pip
+#pip3 install apycula
 
 apt-get -y install \
+    bison \
+    build-essential \
     clang \
+    flex \
+    gawk \
+    git \
+    graphviz \
+    libboost-all-dev \
+    libffi-dev \
+    libreadline-dev \
+    pkg-config \
+    python3 \
+    tcl-dev \
+    xdot \
+    zlib1g-dev
+
+cd /opt/src
+git clone https://github.com/YosysHQ/yosys.git
+cd yosys
+CC=clang CXX=clang++ make
+make install
+
+cd /opt/src
+git clone https://github.com/YosysHQ/nextpnr.git
+
+apt-get -y install \
     clang-format \
     cmake \
     curl \
-    git \
-    libboost-all-dev \
-    libeigen3-dev \
+    libeigen3-dev
 
 #apt-get -y install \
 #    qt5-qmake \
 #    qtbase5-dev \
 #    qtcreator
 
-mkdir /opt/src
 cd /opt/src
 git clone https://github.com/YosysHQ/nextpnr.git
