7ec6f9bf88e8ebbb4c12d0bb3f2a018701f635d8
Evgenii Kliuchnikov <eustas.ru@gmail.com>
2024-04-30 14:40:06 +0200

    build nextpnr

--- .devcontainer/install-deps.sh
+++ .devcontainer/install-deps.sh
@@ -7,45 +7,42 @@ export DEBIAN_FRONTEND=noninteractive
 
 mkdir /opt/src
 
-#apt-get -y install python3-dev python3-pip
-#pip3 install apycula
-
 apt-get -y install \
     bison \
     build-essential \
     clang \
+    clang-format \
+    cmake \
+    curl \
     flex \
     gawk \
     git \
     graphviz \
     libboost-all-dev \
+    libeigen3-dev \
     libffi-dev \
     libreadline-dev \
     pkg-config \
     python3 \
+    python3-dev \
+    python3-pip \
+    qt5-qmake \
+    qtbase5-dev \
+    qtcreator \
     tcl-dev \
     xdot \
     zlib1g-dev
 
+pip3 install apycula
+
 cd /opt/src
 git clone https://github.com/YosysHQ/yosys.git
 cd yosys
-CC=clang CXX=clang++ make
+CC=clang CXX=clang++ make -j`nproc`
 make install
 
 cd /opt/src
 git clone https://github.com/YosysHQ/nextpnr.git
-
-apt-get -y install \
-    clang-format \
-    cmake \
-    curl \
-    libeigen3-dev
-
-#apt-get -y install \
-#    qt5-qmake \
-#    qtbase5-dev \
-#    qtcreator
-
-cd /opt/src
-git clone https://github.com/YosysHQ/nextpnr.git
+cmake . -DARCH="himbaechel" -DHIMBAECHEL_GOWIN_DEVICES="all"
+make -j`nproc`
+make install
